62ecadf09dcf67fb73449c47e5bbdd8173788411
Cody Morley <codymorleyinc@gmail.com>
2020-05-18 14:56:31 -0700

    finished initial file tasks. File tree is closed, clean, and ready to build in.

---
+++ Journal/Journal/Models/CoreData/CoreDataStack.swift
@@ -0,0 +1,10 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Cody Morley on 5/18/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+import CoreData
---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,10 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Cody Morley on 5/18/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+import CoreData
