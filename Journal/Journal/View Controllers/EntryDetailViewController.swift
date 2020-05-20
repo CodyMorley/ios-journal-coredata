0196e9540df419ad3836dc32e33dc32ae879e58e
Cody Morley <codymorleyinc@gmail.com>
2020-05-20 15:07:22 -0700

    finished layout of detail view.

---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Cody Morley on 5/20/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
